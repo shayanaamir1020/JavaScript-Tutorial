ccfce176e7d948bf5249e0e4db919dae7136d137
shaynaamir1020 <shaynaamir1020@gmail.com>
2026-05-16 11:30:24 +0500

    variables practice done

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -2,4 +2,8 @@ const accountId = 16880
 let accountEmail = "khan123@gmail.com"
 var accountPassword = "12123030"
 let accountCity = "Kohat"
-console.log([accountId,accountEmail]);
+let accountState;
+accountEmail = "Awam1257@gmail.com"
+accountPassword = "554488"
+accountCity = "Peshawar"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
