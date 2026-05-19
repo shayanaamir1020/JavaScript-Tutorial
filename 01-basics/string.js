673c0649a451c67389d62e722ee4189136fdfd3b
shaynaamir1020 <shaynaamir1020@gmail.com>
2026-05-19 16:14:35 +0500

    strings practice done

---
+++ 01-basics/string.js
@@ -0,0 +1,30 @@
+const name = "Shayan"
+const repoCount = 20
+console.log(name + repoCount + "Value");
+console.log(`Hi my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String("M.Shayan")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('h'));
+const newString = gameName.substring(0,4)
+console.log(newString);
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+const newStringOne = "  Shayan  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url = "https://Shayan.com/Shayan%20Awan"
+console.log(url.replace("%20","-"));
+console.log(url.includes("Awan"));
+console.log(gameName.split('.'));
+
+
+
+
+
+
+
+
