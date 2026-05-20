8e5047ab6f68e6a822a9a89eb38873acc31801bf
shaynaamir1020 <shaynaamir1020@gmail.com>
2026-05-20 19:59:47 +0500

    arrays practice done

---
+++ 02-basics/01-Arrays.js
@@ -0,0 +1,25 @@
+const myarr = [1,2,3,4,5]
+const myheroes = ["superman","batman","ironman"]
+const myarr2 = new Array(1,2,3,4,5)
+// console.log(myarr[1])
+//Arrays methods
+// myarr.push(9)
+// myarr.pop()
+// myarr.unshift(0)
+// myarr.shift()
+// console.log(myarr.includes(7))
+// console.log(myarr.indexOf(4))
+// const newarr = myarr.join()
+// console.log(newarr)
+// console.log(myarr)
+// slice and splice
+console.log("A ",myarr)
+const mynewarr = myarr.slice(1,4)
+console.log(mynewarr);
+console.log("B ",myarr);
+const mynewarr1 = myarr.splice(1,3)
+console.log("C ", myarr)
+console.log(mynewarr1);
+
+
+
