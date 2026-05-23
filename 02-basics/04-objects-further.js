79bc10e6db8a1fcc935136cce07ceff0f8282298
shaynaamir1020 <shaynaamir1020@gmail.com>
2026-05-23 15:50:08 +0500

    objects-further practice done

---
+++ 02-basics/04-objects-further.js
@@ -0,0 +1,40 @@
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "shani"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser)
+const regularUser = {
+    email:"123@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"M",
+            lastname:"Shayan"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname)
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"c",4:"d"}
+const obj4 ={5:"e",6:"f"}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+const obj3 ={...obj1,...obj2,...obj4}
+// console.log(obj3)
+const users = [
+    {
+        userId: 1,
+        email: "xyz@gmail.com"
+    },
+    {
+        userId: 2,
+        email: "s@gmail.com"
+    }
+];
+users[1].email
+// console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(Object.hasOwnProperty('isLoggedIn'))
+
+
+
