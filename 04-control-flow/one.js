0d20b9372d79f80871e08254ab386139e69ea692
shaynaamir1020 <shaynaamir1020@gmail.com>
2026-06-03 20:28:37 +0500

    control flow practice done

---
+++ 04-control-flow/one.js
@@ -0,0 +1,46 @@
+// if
+const isUserLoggedIn = true;
+const temprature = 40;
+// if (temprature<50){
+//   console.log("temprature is less than 50");
+  
+// }
+// else{console.log("temprature is greater than 50")}
+
+// const score = 200;
+// if(score>100){
+//      const power = "fly"
+//      console.log(`user power: ${power}`);
+     
+// }
+
+// const balance = 1000;
+// if(balance>500) console.log("test");
+
+// if(balance<500){
+//     console.log("balance is less than 500");
+    
+// }else if(balance<750){
+//     console.log("balance is less than 750");
+    
+// }else if(balance<900){
+//     console.log("balance is less than 900");
+    
+// }else{
+//     console.log("balance is less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const userLoggedInFromGoogle = false
+const userLoggedInFromGmail = true
+// if(userLoggedIn  && debitCard ){
+//     console.log("allowed to buy course");
+    
+// }
+
+if (userLoggedInFromGoogle || userLoggedInFromGmail){
+    console.log("user logged in");
+    
+}
