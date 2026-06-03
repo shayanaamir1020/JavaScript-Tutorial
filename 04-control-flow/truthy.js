d2505b257bde00f8792de6520d8d0c1e83da7714
shaynaamir1020 <shaynaamir1020@gmail.com>
2026-06-03 20:32:07 +0500

    truthy,falsy practice done

---
+++ 04-control-flow/truthy.js
@@ -0,0 +1,33 @@
+const userEmail = []
+
+// if(userEmail){
+//     console.log("got user email");
+    
+// }else{
+//     console.log("dont have useremail");
+    
+// }
+
+// if(userEmail.length === 0){
+//     console.log("array is empty");
+    
+// }
+
+// const emptyObject = {}
+// if(Object.keys(emptyObject).length===0){
+//     console.log("object is empty");
+    
+// }
+
+// nullish coalescing operator (??) : null/undefined
+
+let val1;
+// val1 = 7 ?? 10
+// val1 = null ?? 5
+val1 = undefined ?? 20
+console.log(val1);
+
+// terinary operator
+const mintTeaPrice = 100
+mintTeaPrice>=80 ? console.log("price is less than 80") : console.log("price is more than 80");
+
