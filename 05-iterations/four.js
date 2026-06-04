9ef52a4281e3130b997f08bf11ed128fa8b814dd
shaynaamir1020 <shaynaamir1020@gmail.com>
2026-06-04 18:27:51 +0500

    for in and maps practice done

---
+++ 05-iterations/four.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js:"javascript",
+    cpp:"c++",
+    py:"python",
+    rb:"ruby"
+}
+
+for (const key in myObject) {
+    
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+    
+}
+
+const programming = ["js","py","rb","java"]
+for (const key in programming) {
+    console.log(programming[key]);
+        
+}
